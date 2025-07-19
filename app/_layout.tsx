9306a9bcb042cd71b1da34e7a53ba98df1e0cc4f
German <thewhitehaven04@gmail.com>
2025-07-19 21:14:16 +0200

    App layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,31 @@
 "use client"
-import { Stack } from "expo-router"
+import { getColor } from "@/src/colorScheme"
+import Ionicons from "@expo/vector-icons/Ionicons"
+import { Drawer } from "expo-router/drawer"
+import React from "react"
+import { GestureHandlerRootView } from "react-native-gesture-handler"
 
 export default function RootLayout() {
     return (
-        <Stack>
-            <Stack.Screen name="index" />
-            <Stack.Screen name="season/[season]" />
-        </Stack>
+        <GestureHandlerRootView>
+            <Drawer>
+                <Drawer.Screen
+                    options={{
+                        drawerIcon: ({ focused }) => (
+                            <Ionicons name={focused ? "home" : "home-outline"} size={24} />
+                        ),
+                    }}
+                />
+                <Drawer.Screen
+                    name="season/[season]"
+                    options={{
+                        title: `Season calendar`,
+                        sceneStyle: {
+                            backgroundColor: getColor("background"),
+                        },
+                    }}
+                />
+            </Drawer>
+        </GestureHandlerRootView>
     )
 }
