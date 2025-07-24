6ea2ae3616d398c5647bd13c968efe1e51ce3212
German <thewhitehaven04@gmail.com>
2025-07-24 20:54:21 +0200

    Season data refresh gesture

--- app/season/[season]/index.tsx
+++ app/season/[season]/index.tsx
@@ -2,12 +2,14 @@
 import renderSeasonEventsAction from "@/src/actions/render/seasonEvents"
 import { Button } from "@/src/components/ui/Button"
 import { LoadingSpinner } from "@/src/components/ui/LoadingSpinner"
-import { Link, useLocalSearchParams } from "expo-router"
-import { Suspense, useMemo } from "react"
-import { ScrollView, StyleSheet, Text } from "react-native"
+import { Link, useLocalSearchParams, useRouter } from "expo-router"
+import { Suspense, useMemo, useState } from "react"
+import { ScrollView, StyleSheet, Text, View } from "react-native"
+import { Gesture, GestureDetector } from "react-native-gesture-handler"
+import { useSharedValue } from "react-native-reanimated"
 
 const style = StyleSheet.create({
-    wrapContainer: {
+    wrapperContent: {
         alignItems: "center",
         justifyContent: "flex-start",
         gap: 16,
@@ -19,23 +21,57 @@ const style = StyleSheet.create({
     },
 })
 
+const PAGE_REFRESH_SCROLL_THRESHOLD = 75
+const VELOCITY_THRESHOLD = 75
+
 export default function SeasonScreen() {
     const { season }: { season: string } = useLocalSearchParams()
 
     const seasonEvents = useMemo(() => renderSeasonEventsAction({ season }), [season])
 
+    const { replace } = useRouter()
+
+    const isSufficientlyPanned = useSharedValue(false)
+
+    const scrollUp = useMemo(
+        () =>
+            Gesture.Pan()
+                .minDistance(10)
+                .onUpdate((event) => {
+                    console.log(event.translationY)
+                    if (
+                        event.translationY >= PAGE_REFRESH_SCROLL_THRESHOLD &&
+                        event.velocityY >= VELOCITY_THRESHOLD
+                    ) {
+                        isSufficientlyPanned.value = true
+                        replace(`/season/${season}`, {
+                            relativeToDirectory: true
+                        })
+                    }
+                })
+                .onEnd(() => {
+                    if (isSufficientlyPanned) {
+                        isSufficientlyPanned.value = false
+                    }
+                })
+                .runOnJS(true),
+        [isSufficientlyPanned, replace, season],
+    )
+
     return (
-        <ScrollView contentContainerStyle={style.wrapContainer}>
-            <Link href={`/season/${season}/selectorModal`} asChild>
-                <Button>
-                    <Text>{season}</Text>
-                </Button>
-            </Link>
-            <Suspense fallback={<LoadingSpinner />}>
-                <ScrollView contentContainerStyle={style.scrollContainer}>
-                    {seasonEvents}
-                </ScrollView>
-            </Suspense>
-        </ScrollView>
+        <GestureDetector gesture={scrollUp}>
+            <ScrollView contentContainerStyle={style.wrapperContent}>
+                <Link href={`/season/${season}/selectorModal`} asChild>
+                    <Button>
+                        <Text>{season}</Text>
+                    </Button>
+                </Link>
+                <Suspense fallback={<LoadingSpinner />}>
+                    <ScrollView contentContainerStyle={style.scrollContainer}>
+                        {seasonEvents}
+                    </ScrollView>
+                </Suspense>
+            </ScrollView>
+        </GestureDetector>
     )
 }
