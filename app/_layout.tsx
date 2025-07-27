c160afc814093f059c1a08791aec91b36508722f
German <thewhitehaven04@gmail.com>
2025-07-27 23:32:05 +0200

    Initial implementation of the laps screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,8 @@
 "use client"
 import { getColor } from "@/src/colorScheme"
+import { Providers } from "@/src/providers"
 import Stack from "expo-router/build/layouts/Stack"
 import React from "react"
-import { SafeAreaView } from 'react-native'
 import { GestureHandlerRootView } from "react-native-gesture-handler"
 
 export const unstable_settings = {
@@ -11,35 +11,46 @@ export const unstable_settings = {
 export default function RootLayout() {
     return (
         <GestureHandlerRootView>
-            <Stack>
-                <Stack.Screen name="index" />
-                <Stack.Screen
-                    name="season/[season]/index"
-                    options={{
-                        title: `Season calendar`,
-                        contentStyle: {
-                            backgroundColor: getColor("background"),
-                        },
-                    }}
-                />
-                <Stack.Screen
-                    name="season/[season]/selectorModal"
-                    options={{
-                        presentation: "containedTransparentModal",
-                        animation: "fade",
-                        title: "Select season",
-                    }}
-                />
-                <Stack.Screen
-                    name="season/[season]/event/[event]/session/[session]/results"
-                    options={({ route }) => ({
-                        title: `${route.params.event} - ${route.params.session} results`,
-                        contentStyle: {
-                            backgroundColor: getColor("background"),
-                        },
-                    })}
-                />
-            </Stack>
+            <Providers>
+                <Stack>
+                    <Stack.Screen name="index" />
+                    <Stack.Screen
+                        name="season/[season]/index"
+                        options={{
+                            title: `Season calendar`,
+                            contentStyle: {
+                                backgroundColor: getColor("background"),
+                            },
+                        }}
+                    />
+                    <Stack.Screen
+                        name="season/[season]/selectorModal"
+                        options={{
+                            presentation: "containedTransparentModal",
+                            animation: "fade",
+                            title: "Select season",
+                        }}
+                    />
+                    <Stack.Screen
+                        name="season/[season]/event/[event]/session/[session]/results"
+                        options={({ route }) => ({
+                            title: `${route.params?.event ?? ""} - ${route.params?.session ?? ""} results`,
+                            contentStyle: {
+                                backgroundColor: getColor("background"),
+                            },
+                        })}
+                    />
+                    <Stack.Screen
+                        name="season/[season]/event/[event]/session/[session]/laps"
+                        options={({ route }) => ({
+                            title: `${route.params?.event ?? ""} - ${route.params?.session ?? ""} laps`,
+                            contentStyle: {
+                                backgroundColor: getColor("background"),
+                            },
+                        })}
+                    />
+                </Stack>
+            </Providers>
         </GestureHandlerRootView>
     )
 }
--- app/season/[season]/event/[event]/session/[session]/laps.tsx
+++ app/season/[season]/event/[event]/session/[session]/laps.tsx
@@ -1,13 +1,117 @@
 "use client"
 import { useLocalSearchParams } from "expo-router"
-import { View, Text } from "react-native"
+import { View, Text, StyleSheet } from "react-native"
+import { useSuspenseQuery } from "@tanstack/react-query"
+import { ApiClient } from "@/src/client"
+import { getSessionLaptimesFilteredApiSeasonYearEventEventSessionSessionLapsPost } from "@/src/client/generated"
+import { Suspense, useState } from "react"
+import { LoadingSpinner } from "@/src/components/ui/LoadingSpinner"
+import { Button } from "@/src/components/ui/Button"
+import { LapCard } from "@/src/components/Tables/presets/laps"
+import Ionicons from "@expo/vector-icons/Ionicons"
+
+const styleSheet = StyleSheet.create({
+    wrapper: {
+        width: "100%",
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-around",
+        paddingTop: 8,
+    },
+    button: {
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+    },
+    lapList: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "stretch",
+        gap: 8,
+        padding: 8,
+    },
+})
 
 export default function LapsScreen() {
-    const params = useLocalSearchParams()
+    const {
+        season,
+        session,
+        event,
+        drivers,
+    }: { season: string; session: string; event: string; drivers: string[] } =
+        useLocalSearchParams()
+
+    const queries = drivers.map((driver) => ({
+        driver,
+        lap_filter: null,
+    }))
+    const [selectedDriverIndex, setDriverIndex] = useState(0)
+
+    const { data } = useSuspenseQuery({
+        queryKey: [season, session, event, drivers],
+        queryFn: async () =>
+            await getSessionLaptimesFilteredApiSeasonYearEventEventSessionSessionLapsPost({
+                client: ApiClient,
+                path: {
+                    event,
+                    session,
+                    year: season,
+                },
+                body: { queries },
+                throwOnError: true,
+            }),
+    })
+
+    const selectedDriverData = data.data.driver_lap_data[selectedDriverIndex].laps
+    const currentDriver = data.data.driver_lap_data[selectedDriverIndex].driver
+
+    const hasLeft = selectedDriverIndex > 0
+    const hasRight = selectedDriverIndex < data.data.driver_lap_data.length - 1
 
     return (
-        <View>
-            <Text>{JSON.stringify(params)}</Text>
-        </View>
+        <Suspense fallback={<LoadingSpinner />}>
+            <View style={styleSheet.wrapper}>
+                {hasLeft && (
+                    <Button
+                        style={styleSheet.button}
+                        onPress={() => setDriverIndex(selectedDriverIndex - 1)}
+                    >
+                        <Ionicons name="chevron-back-outline" />
+                        <Text>{data.data.driver_lap_data[selectedDriverIndex - 1].driver}</Text>
+                    </Button>
+                )}
+                <View>
+                    <Text>{currentDriver}</Text>
+                </View>
+                {hasRight && (
+                    <Button
+                        style={styleSheet.button}
+                        onPress={() => setDriverIndex(selectedDriverIndex + 1)}
+                    >
+                        <Text>{data.data.driver_lap_data[selectedDriverIndex + 1].driver}</Text>
+                        <Ionicons name="chevron-forward-outline" />
+                    </Button>
+                )}
+            </View>
+            <View style={styleSheet.lapList}>
+                {selectedDriverData.map((driverData, index) => (
+                    <LapCard
+                        key={driverData.id}
+                        isPersonalBest={driverData.is_pb}
+                        isSessionBest={false}
+                        lapNumber={index + 1}
+                        laptime={driverData.laptime}
+                        s1={driverData.sector_1_time}
+                        s2={driverData.sector_2_time}
+                        s3={driverData.sector_3_time}
+                        st1={driverData.speedtrap_1}
+                        st2={driverData.speedtrap_2}
+                        stFL={driverData.speedtrap_fl}
+                        compound={driverData.compound_id}
+                    />
+                ))}
+            </View>
+        </Suspense>
     )
 }
