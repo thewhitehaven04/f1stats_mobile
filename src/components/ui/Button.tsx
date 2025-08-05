1944569db6bd8f1b1eb53b7a543a23895ea1a5be
German <thewhitehaven04@gmail.com>
2025-08-05 21:12:51 +0200

    Button animation

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,12 +1,12 @@
 "use client"
 import { getColor } from "@/src/colorScheme"
-import { type ComponentProps } from "react"
+import { type ComponentProps, type ReactNode } from "react"
 import { Pressable, type StyleProp, type ViewStyle } from "react-native"
 import { Text } from "react-native"
 import * as FontSizes from "@/src/fontSizes"
 import Animated, { useAnimatedStyle, useSharedValue, withTiming } from "react-native-reanimated"
 
-const ANIMATION_DURATION_MS = 90 
+const ANIMATION_DURATION_MS = 90
 
 export const Button = ({
     children,
@@ -15,16 +15,17 @@ export const Button = ({
     style,
     label,
     ...rest
-}: Omit<ComponentProps<typeof Pressable>, "style"> & {
+}: Omit<ComponentProps<typeof Pressable>, "style" | "children"> & {
     variant?: "outline" | "solid"
     size?: "regular" | "large"
     style?: StyleProp<ViewStyle>
     label?: string
+    children: ReactNode
 }) => {
     const svScale = useSharedValue(1)
 
     const animatedStyle = useAnimatedStyle(() => ({
-        transform: [{ scale: withTiming(svScale.value, { duration: 150 })}],
+        transform: [{ scale: svScale.value }],
     }))
     const handlePress = () => {
         svScale.value = withTiming(0.98, { duration: ANIMATION_DURATION_MS })
@@ -33,9 +34,16 @@ export const Button = ({
         }, ANIMATION_DURATION_MS)
     }
     return (
-        <Animated.View style={animatedStyle}>
-            <Pressable
-                {...rest}
+        <Pressable
+            {...rest}
+            onPress={(evt) => {
+                if (rest.onPress) {
+                    rest.onPress(evt)
+                }
+                handlePress()
+            }}
+        >
+            <Animated.View
                 style={[
                     {
                         paddingBlock: size === "regular" ? 8 : 16,
@@ -51,9 +59,8 @@ export const Button = ({
                         flexDirection: "row",
                         justifyContent: "center",
                     },
-                    style,
+                    animatedStyle,
                 ]}
-                onPress={() => handlePress()}
             >
                 {label ? (
                     <Text
@@ -67,7 +74,7 @@ export const Button = ({
                 ) : (
                     children
                 )}
-            </Pressable>
-        </Animated.View>
+            </Animated.View>
+        </Pressable>
     )
 }
